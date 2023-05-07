2ef5fe43d80d6a38bf47c2ce082368f8f365e8d1
Celestine Nwachukwu <celexspencer007@gmail.com>
2023-05-07 01:00:43 +0100

    fixed favourite page issue

--- src/pages/FavouritesPage.jsx
+++ src/pages/FavouritesPage.jsx
@@ -5,33 +5,37 @@ import SearchHotelCard from "../components/cards/SearchHotelCard";
 
 function FavouritesPage() {
     const loaderData = useLoaderData();
-    let favourites = <Typography
-        variant="h5"
-        align="center"
-        sx={{
-            display: "flex",
-            justifyContent: 'center',
-            alignItems: "center",
-            gap: 2
-        }}>
-        No favourites yet
-        <Favorite />
-    </Typography>
+    let favourites = Object.values(loaderData).filter(hotel => hotel?.isFav === true).map(data => <SearchHotelCard
+        key={data.id}
+        id={data.id}
+        title={data.name}
+        city={data.city}
+        hero={data.images.hero}
+        location={data.location}
+        rating={data.rating}
+        isFav={data.isFav}
+        basePrice={data.rooms.single || data.rooms.double}
+    />)
 
-    if (loaderData) {
-        favourites = Object.values(loaderData).filter(hotel => hotel?.isFav === true).map(data => <SearchHotelCard
-            key={data.id}
-            id={data.id}
-            title={data.name}
-            city={data.city}
-            hero={data.images.hero}
-            location={data.location}
-            rating={data.rating}
-            isFav={data.isFav}
-            basePrice={data.rooms.single || data.rooms.double}
-        />)
+    if (!favourites.length) {
+        favourites = <Typography
+            variant="h5"
+            align="center"
+            sx={{
+                display: "flex",
+                justifyContent: 'center',
+                alignItems: "center",
+                gap: 2
+            }}>
+            No favourites yet
+            <Favorite />
+        </Typography>
+
+
 
     }
+
+
     return (
         <Stack component="section" className="section" gap={6}>
             <Typography variant="h4">Favourites</Typography>
